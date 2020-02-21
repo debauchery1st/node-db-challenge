8b06f6d6a103241095824381f4f01e8aae93f37f
debauchery1st <trevor.j.martin@gmail.com>
2020-02-21 13:29:51 -0500

    updateTask

--- Tasks/task-model.js
+++ Tasks/task-model.js
@@ -3,7 +3,8 @@ const db = require("../data/db-config.js");
 module.exports = {
   getTasks,
   getTasksById,
-  addTask
+  addTask,
+  updateTask
 };
 
 function getTasks() {
@@ -14,8 +15,26 @@ function getTasksById(id) {
   return db("task").where({ id });
 }
 
-function addTask(task) {
+function addTask(task, project_id, resource_id) {
+  if (!project_id) {
+    return { error: "every task requires a project" };
+  }
   return db("task")
     .insert(task, "id")
-    .then(t => getProjectById(t.id));
+    .then(t =>
+      db("project_tasks").insert({ project_id, resource_id, task_id: t.id })
+    )
+    .then(getTasksById(t.id));
+}
+
+function updateTask(task, id, project_id, resource_id) {
+  if (!id && !task.id) {
+    return { error: "task id required" };
+  }
+  if (!project_id) {
+    return { error: "project id required for update." };
+  }
+  return db("task")
+    .update(task)
+    .where({ id });
 }
